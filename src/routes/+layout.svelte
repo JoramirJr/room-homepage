ba1a8035bb83fec5c794c2505c22767bb692d493
Joramir da Silva Junior <joramirjunior@gmail.com>
2024-04-13 18:14:36 -0300

    1st commit, starting home screen, mobile-first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "../app.css";
+</script>
+
+<header class="h-header bg-red-600 flex items-center pt-[1rem] px-[5.5vw]">
+  <button>
+    <img
+      src="/icon-hamburger.svg"
+      alt="Menu Hamburger Icon"
+      aria-label="Mobile Menu Button"
+      class="w-[1.25rem]"
+    />
+  </button>
+  <h1 class="w-full text-center align-text-top text-[1.5rem]">room</h1>
+</header>
+
+<slot />
