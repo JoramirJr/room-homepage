3b8627a63262748457e4d1156b723770f59067dd
JoramirJr <joramirjunior@gmail.com>
2024-04-15 09:43:53 -0300

    mobile screen, done, without interactivity

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,4 @@
   import "../app.css";
 </script>
 
-<header class="fixed w-full h-header bg-transparent flex items-center pt-[1rem] px-[5.5vw]">
-  <button>
-    <img
-      src="/icon-hamburger.svg"
-      alt="Menu Hamburger Icon"
-      aria-label="Mobile Menu Button"
-      class="w-[1.25rem]"
-    />
-  </button>
-  <h1 class="w-full text-center align-text-top text-[1.75rem] font-spartan font-medium">room</h1>
-</header>
-
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,20 @@
+<header
+  class="absolute left-0 top-0 w-full h-header bg-transparent flex items-center pt-[1rem] px-[5.5vw]"
+>
+  <button>
+    <img
+      src="/icon-hamburger.svg"
+      alt="Menu Hamburger Icon"
+      aria-label="Mobile Menu Button"
+      class="w-[1.25rem]"
+    />
+  </button>
+  <h1
+    class="w-full text-center align-text-top text-[1.75rem] font-spartan font-medium"
+  >
+    room
+  </h1>
+</header>
 <main class="h-screen w-screen">
   <section class="flex flex-wrap *:w-screen">
     <div class="h-[22rem] bg-light-furniture flex items-end justify-end">
@@ -20,23 +37,49 @@
         </button>
       </div>
     </div>
-    <div class="grid place-items-center bg-white h-[24rem] p-[1.75rem]">
+    <div
+      class="grid place-items-center h-[24rem] px-[1.75rem] box-content my-[3rem]"
+    >
       <h2
         class="text-black text-[2.5rem] leading-[2.25rem] font-semibold tracking-tight"
       >
         Discover innovative ways to decorate
       </h2>
-      <span class="text-gray-400 leading-[1.5rem]" >
+      <span class="text-gray-400 leading-[1.25rem]">
         We provide unmatched quality, comfort, and style for property owners
         across the country. Our experts combine form and function in bringing
         your vision to life. Create a room in your own style with our collection
         and make your property a reflection of you and what you love.
       </span>
-      <button class="my-[2rem] flex items-center gap-[1rem] text-black text-[1rem] tracking-[1rem] w-full" >
+      <button
+        class="flex text-[1rem] items-center gap-[1rem] text-black text-[1rem] tracking-[1rem] w-full"
+      >
         SHOP NOW
-        <img class="h-[.75rem]" src="icon-arrow.svg" alt="shop now arrow icon" />
+        <img
+          class="h-[.75rem]"
+          src="icon-arrow.svg"
+          alt="shop now arrow icon"
+        />
       </button>
     </div>
-  </section> 
-  <section class="grid grid-cols-3"></section>
+  </section>
+  <section>
+    <img src="image-about-dark.jpg" alt="" />
+    <div class="h-fit px-[2rem] py-[2rem] box-content">
+      <h2
+        class="text-black text-[.9rem] leading-[3.25rem] font-bold tracking-[.4rem]"
+      >
+        ABOUT OUR FURNITURE
+      </h2>
+      <span class="text-gray-400 leading-[1.25rem]">
+        Our multifunctional collection blends design and function to suit your
+        individual taste. Make each room unique, or pick a cohesive theme that
+        best express your interests and what inspires you. Find the furniture
+        pieces you need, from traditional to contemporary styles or anything in
+        between. Product specialists are available to help you create your dream
+        space.
+      </span>
+    </div>
+    <img src="image-about-light.jpg" alt="" />
+  </section>
 </main>
